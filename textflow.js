09e2d32f0deeaeeae10c465548f1b22af887258f
Robert Dickinson <bob.dickinson@gmail.com>
2016-04-11 18:31:02 -0700

    Updated textflow sample for multiline.

--- textflow.js
+++ textflow.js
@@ -6,9 +6,15 @@ exports.View =
     elements:
     [
         { control: "stackpanel", orientation: "Vertical", width: "*", contents: [
-            { control: "edit", binding: "userText" },
+            { control: "stackpanel", orientation: "Horizontal", contents: [
+                { control: "text", value: "Size", fontsize: 10, verticalAlignment: "Center", width: 100 },
+                { control: "slider", minimum: 10, maximum: 20, binding: "fontsize", width: 300 },
+            ] },
+            { control: "edit", fontsize: "{fontsize}", placeholder: "Single Line", width: 200, binding: "userText" },
             { control: "text", value: "{userText}", fontsize: 12, width: "*" },
             { control: "rectangle", height: "100", width: "*", fill: "Red", border: "Blue", borderThickness: 5 },
+            { control: "edit", multiline: true, placeholder: "100 tall", width: 200, height: 100, fontsize: "{fontsize}", binding: "userMultilineText"},
+            { control: "edit", multiline: true, placeholder: "3 lines tall", width: 200, lines: 3, fontsize: "{fontsize}", binding: "userMultilineText"},
             ]
         }
     ]
@@ -17,7 +23,9 @@ exports.View =
 exports.InitializeViewModel = function (context, session) {
     var viewModel =
     {
-        userText: "Sample Text"
+        userText: "Sample Text",
+        userMultilineText: "",
+        fontsize: 14
     }
     return viewModel;
 }
